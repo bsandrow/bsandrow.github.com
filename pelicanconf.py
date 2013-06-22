015ca2bc4ee384acdbce6fce0436e69b972e4b4b
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-06-22 00:49:43 -0400

    Tweaking the setup for publishing

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,8 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Brandon Sandrowicz'
 SITENAME = u'Brandon Sandrowicz'
-#SITEURL = 'http://brandon.sandrowicz.org'
-SITEURL = 'http://localhost:8000'
+SITEURL = 'http://brandon.sandrowicz.org'
 
 SITE_LOGO_URL = '/static/images/avatar.jpg'
 
@@ -23,8 +22,8 @@ TRANSLATION_FEED_ATOM = None
 STATIC_PATHS = [ 'pdfs', 'images' ]
 THEME = './theme'
 MENUITEMS = (
-    ("Home", SITEURL),
-    ("Resume", SITEURL + "/static/pdfs/BrandonSandrowicz-resume.pdf"),
+    ("Home", "/"),
+    ("Resume", "/static/pdfs/BrandonSandrowicz-resume.pdf"),
 )
 
 # Blogroll
@@ -36,7 +35,7 @@ SOCIAL = None
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 
 FILES_TO_COPY = (
     ('extras/CNAME','CNAME'),
